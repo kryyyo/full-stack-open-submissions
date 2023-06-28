8299996c650aa6a2210478b7b8c0a9b1f3b93a66
kryo <collene.desilva@gmail.com>
2023-06-29 03:28:16 +0800

    feat: add ex2.11

--- part2/the-phonebook/src/components/Persons.js
+++ part2/the-phonebook/src/components/Persons.js
@@ -1,7 +1,7 @@
 const Persons = ({ filteredPersons }) => {
   return (
     <>
-      {filteredPersons.map(person => <div key={person.id}>{person.name} {person.phone}</div>)}
+      {filteredPersons.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
     </>
   )
 }

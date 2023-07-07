b83c620a6243ef8354442e9e0392e41dba3966ef
kryo <collene.desilva@gmail.com>
2023-07-08 07:17:09 +0800

    feat: part3 - add ex3.6

--- part3/phonebook-be/index.js
+++ part3/phonebook-be/index.js
@@ -66,10 +66,17 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
   const body = req.body
   const hasNameAndNumber = body.name && body.number;
+  const existingPerson = persons.find(person => person.name === body.name);
 
   if (!hasNameAndNumber) {
     return res.status(400).json({
       error: 'content missing'
+    })
+  }
+
+  if (existingPerson) {
+    return res.status(409).json({
+      error: 'name must be unique'
     })
   }
 
